3e6210a1b79fcf576f781fd33cf321f210622e23
Pro69Yes <120266289+Pro69Yes@users.noreply.github.com>
2023-03-11 23:19:49 +0700

    Update GarbageHub [Deflect].lua

--- GarbageHub [Deflect].lua
+++ GarbageHub [Deflect].lua
@@ -1,32 +1,5 @@
 
 
-getgenv().Toggle = false
-function AutoDeflect()
-getgenv().BallDirection = Vector3.new(999999, 999999, 999999)
-
-local plr = game.Players.LocalPlayer
-local ball = false
-
-local function getBall()
-while true do game:GetService("RunService").Stepped:Wait()
-if workspace:FindFirstChild("Ball") ~= nil then
-ball = workspace.Ball
-end
-end
-end
-
-task.spawn(getBall)
-
-while Toggle do game:GetService("RunService").Stepped:Wait()
-if (ball and ball:FindFirstChild("Main") and plr.Character ~= nil) then
-local distance = plr:DistanceFromCharacter(ball.Main.Position)
-plr.Character:WaitForChild("HumanoidRootPart").CFrame = CFrame.lookAt(plr.Character.HumanoidRootPart.Position, Vector3.new(ball.Main.Position.X, plr.Character.HumanoidRootPart.Position.Y, ball.Main.Position.Z))
-if (distance <= 17.8) then
-plr.Character:WaitForChild("Deflection").Remote:FireServer("Deflect", BallDirection)
-end
-end
-end
-       end
 
 ------------------------------------------------[Library Section]------------------------------------------------
 local Rayfield = loadstring(game:HttpGet('https://raw.githubusercontent.com/shlexware/Rayfield/main/source'))()
@@ -70,12 +43,36 @@ local Tab = OrionWindow:MakeTab({
 local Section = Tab:AddSection({
 	Name = "Main Option"
 })
-Tab:AddButton({
+Tab:AddToggle({
 	Name = "AutoDeflect",
-	Callback = function()      		
-getgenv().Toggle = Value
-AutoDeflect()
-  	end    
+	Default = false,
+	Callback = function(Value)
+		getgenv().Toggle = Value
+getgenv().BallDirection = Vector3.new(999999, 999999, 999999)
+
+local plr = game.Players.LocalPlayer
+local ball = false
+
+local function getBall()
+while true do game:GetService("RunService").Stepped:Wait()
+if workspace:FindFirstChild("Ball") ~= nil then
+ball = workspace.Ball
+end
+end
+end
+
+task.spawn(getBall)
+
+while Toggle do game:GetService("RunService").Stepped:Wait()
+if (ball and ball:FindFirstChild("Main") and plr.Character ~= nil) then
+local distance = plr:DistanceFromCharacter(ball.Main.Position)
+plr.Character:WaitForChild("HumanoidRootPart").CFrame = CFrame.lookAt(plr.Character.HumanoidRootPart.Position, Vector3.new(ball.Main.Position.X, plr.Character.HumanoidRootPart.Position.Y, ball.Main.Position.Z))
+if (distance <= 17.8) then
+plr.Character:WaitForChild("Deflection").Remote:FireServer("Deflect", BallDirection)
+end
+end
+end
+	end    
 })
 OrionLib:Init()
 
