c4a0570c54ad5f2dfc497ad36382410644efefc0
Pro69Yes <120266289+Pro69Yes@users.noreply.github.com>
2023-03-12 16:06:55 +0700

    Update GarbageHub [Deflect].lua

--- GarbageHub [Deflect].lua
+++ GarbageHub [Deflect].lua
@@ -2,36 +2,10 @@
 
 
 ------------------------------------------------[Library Section]------------------------------------------------
-local Rayfield = loadstring(game:HttpGet('https://raw.githubusercontent.com/shlexware/Rayfield/main/source'))()
 local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/shlexware/Orion/main/source')))()
 
 ------------------------------------------------[KeySystem]------------------------------------------------
-local Window = Rayfield:CreateWindow({
-   Name = "GarbageHub Loader",
-   LoadingTitle = "Loading . . . .",
-   LoadingSubtitle = "By ForgorMoment",
-   ConfigurationSaving = {
-      Enabled = false,
-      FolderName = nil, -- Create a custom folder for your hub/game
-      FileName = "Big Hub"
-   },
-   Discord = {
-      Enabled = false,
-      Invite = "noinvitelink", -- The Discord invite code, do not include discord.gg/. E.g. discord.gg/ABCD would be ABCD.
-      RememberJoins = true -- Set this to false to make them join the discord every time they load it up
-   },
-   KeySystem = true, -- Set this to true to use our key system
-   KeySettings = {
-      Title = "Garbage Hub",
-      Subtitle = "Key System",
-      Note = "Key : forgor",
-      FileName = "SiriusKey",
-      SaveKey = true,
-      GrabKeyFromSite = false, -- If this is true, set Key below to the RAW site you would like Rayfield to get the key from
-      Key = "forgor"
-   }
-})
-Rayfield:Destroy()
+loadstring(game:HttpGet('https://raw.githubusercontent.com/Pro69Yes/GarbageHub/main/GarbageHub%20Keysystem'))()
 ------------------------------------------------[Windows]------------------------------------------------
 local OrionWindow = OrionLib:MakeWindow({Name = "DEFLECT 🙅‍♂️ | GarbageHub", HidePremium = false, SaveConfig = false, ConfigFolder = "OrionTest"})
 ------------[Tab Section]------------
@@ -44,7 +18,7 @@ local Section = Tab:AddSection({
 	Name = "Main Option"
 })
 Tab:AddToggle({
-	Name = "AutoDeflect",
+	Name = "AutoDeflect | 95% Deflect 5% die",
 	Default = false,
 	Callback = function(Value)
 		getgenv().Toggle = Value
