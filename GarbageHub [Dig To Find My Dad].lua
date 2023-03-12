71ab3b8eb872b9da462fbc6f798fc519318cbd00
Pro69Yes <120266289+Pro69Yes@users.noreply.github.com>
2023-03-12 16:08:21 +0700

    Update GarbageHub [Dig To Find My Dad].lua

--- GarbageHub [Dig To Find My Dad].lua
+++ GarbageHub [Dig To Find My Dad].lua
@@ -97,36 +97,10 @@ end
 
 
 ------------------------------------------------[Library Section]------------------------------------------------
-local Rayfield = loadstring(game:HttpGet('https://raw.githubusercontent.com/shlexware/Rayfield/main/source'))()
 local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/shlexware/Orion/main/source')))()
 
 ------------------------------------------------[KeySystem]------------------------------------------------
-local Window = Rayfield:CreateWindow({
-   Name = "GarbageHub Loader",
-   LoadingTitle = "Loading . . . .",
-   LoadingSubtitle = "By ForgorMoment",
-   ConfigurationSaving = {
-      Enabled = false,
-      FolderName = nil, -- Create a custom folder for your hub/game
-      FileName = "Big Hub"
-   },
-   Discord = {
-      Enabled = false,
-      Invite = "noinvitelink", -- The Discord invite code, do not include discord.gg/. E.g. discord.gg/ABCD would be ABCD.
-      RememberJoins = true -- Set this to false to make them join the discord every time they load it up
-   },
-   KeySystem = true, -- Set this to true to use our key system
-   KeySettings = {
-      Title = "Garbage Hub",
-      Subtitle = "Key System",
-      Note = "Key : forgor",
-      FileName = "SiriusKey",
-      SaveKey = true,
-      GrabKeyFromSite = false, -- If this is true, set Key below to the RAW site you would like Rayfield to get the key from
-      Key = "forgor"
-   }
-})
-Rayfield:Destroy()
+loadstring(game:HttpGet('https://raw.githubusercontent.com/Pro69Yes/GarbageHub/main/GarbageHub%20Keysystem'))()
 ------------------------------------------------[Windows]------------------------------------------------
 local OrionWindow = OrionLib:MakeWindow({Name = "Garbage Hub ([1MVISITS!] Dig To Find My Dad)", HidePremium = false, SaveConfig = false, ConfigFolder = "OrionTest"})
 
@@ -153,7 +127,7 @@ Tab:AddButton({
 })
 Tab:AddTextbox({
 	Name = "Free Money",
-	Default = "Put Anything U want",
+	Default = "",
 	TextDisappear = true,
 	Callback = function(Value)
 		local args = {
